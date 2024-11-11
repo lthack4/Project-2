955a0988b369181cd864eba1b96614841832f20c
thomasn1003 <thomas1003nguyen@gmail.com>
2024-11-10 19:01:48 -0800

    done

--- project2_starter.cpp
+++ project2_starter.cpp
@@ -13,73 +13,139 @@ struct TimeSlot {
 };
 
 int convertToMinutes(const string& time) {
+
     int hours, minutes;
+
     sscanf(time.c_str(), "%d:%d", &hours, &minutes);
+
     return hours * 60 + minutes;
+
 }
+
+
 
 string convertMinutesToHour(int minutes) {
+
     int hours = minutes / 60;
+
     int mins = minutes % 60;
+
     ostringstream oss;
+
     oss << hours << ":" << setw(2) << setfill('0') << mins;
+
     return oss.str();
+
 }
+
+
 
 vector<TimeSlot> updateFunction(const vector<TimeSlot>& schedule, const TimeSlot& dailyAct) {
+
     vector<TimeSlot> updatedSchedule;
+
     updatedSchedule.push_back({0, dailyAct.start}); // Early unavailable period
+
     updatedSchedule.insert(updatedSchedule.end(), schedule.begin(), schedule.end());
+
     updatedSchedule.push_back({dailyAct.end, 1439}); // Late unavailable period (23:59 in minutes)
+
     return updatedSchedule;
+
 }
+
+
 
 vector<TimeSlot> MergeFunction(const vector<TimeSlot>& schedule1, const vector<TimeSlot>& schedule2) {
+
     vector<TimeSlot> merged;
+
     size_t i = 0, j = 0;
+
     while (i < schedule1.size() && j < schedule2.size()) {
+
         if (schedule1[i].start <= schedule2[j].start) {
+
             merged.push_back(schedule1[i++]);
+
         } else {
+
             merged.push_back(schedule2[j++]);
+
         }
+
     }
+
     while (i < schedule1.size()) merged.push_back(schedule1[i++]);
+
     while (j < schedule2.size()) merged.push_back(schedule2[j++]);
+
     return merged;
+
 }
+
+
 
 vector<TimeSlot> softFunction(const vector<TimeSlot>& schedule) {
+
     vector<TimeSlot> sortedSchedule = schedule;
+
     sort(sortedSchedule.begin(), sortedSchedule.end(), [](const TimeSlot& a, const TimeSlot& b) {
+
         return a.start < b.start;
+
     });
+
     return sortedSchedule;
+
 }
 
+
+
 vector<TimeSlot> matchedAvailabilities(const vector<TimeSlot>& schedule, int duration) {
+
     vector<TimeSlot> availabilities;
+
     int end_time_of_last_meeting = schedule[0].end;
 
+
     for (size_t i = 1; i < schedule.size(); ++i) {
+
         int start_time_of_next_meeting = schedule[i].start;
+
         if (start_time_of_next_meeting - end_time_of_last_meeting >= duration) {
+
             availabilities.push_back({end_time_of_last_meeting, start_time_of_next_meeting});
+
         }
+
         end_time_of_last_meeting = max(end_time_of_last_meeting, schedule[i].end);
+
     }
 
     return availabilities;
+
 }
 
+
+
 vector<TimeSlot> groupSchedule(const vector<TimeSlot>& person1, const TimeSlot& person1Avil,
+
                                const vector<TimeSlot>& person2, const TimeSlot& person2Avil, int duration) {
+
     vector<TimeSlot> updatedSchedule1 = updateFunction(person1, person1Avil);
+
     vector<TimeSlot> updatedSchedule2 = updateFunction(person2, person2Avil);
+
     vector<TimeSlot> mergedSchedule = MergeFunction(updatedSchedule1, updatedSchedule2);
+
     vector<TimeSlot> sortedSchedules = softFunction(mergedSchedule);
+
     return matchedAvailabilities(sortedSchedules, duration);
+
 }
+
+
 
 int main() {
     // ============================== TEST CASE 1 ==============================
